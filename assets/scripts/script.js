b1b03fe8ccae91c3fe7e11a1d201929260aa88d4
Andrew Read <aj.read13@gmail.com>
2021-12-21 12:03:34 -0500

    updated css with root color pallet,linked css to html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -25,23 +25,23 @@ getOmdbApi(apiURL);
 
 
 
-var comicVINEAPIKey = "1c73a613612933ec05e589eae31bc83d97ed2e1c"
+// var comicVINEAPIKey = "1c73a613612933ec05e589eae31bc83d97ed2e1c"
 
-var getComics = function (user) {
-    var comicAPI =  "https://comicvine.gamespot.com/api/volumes?api_key=1c73a613612933ec05e589eae31bc83d97ed2e1c"
+// var getComics = function (user) {
+//     var comicAPI =  "https://comicvine.gamespot.com/api/volumes?api_key=1c73a613612933ec05e589eae31bc83d97ed2e1c"
   
-    fetch(comicAPI)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            displayComics(data, user);
-            console.log(data);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to Comic Vine');
-      });
-  };getComics();
+//     fetch(comicAPI)
+//       .then(function (response) {
+//         if (response.ok) {
+//           response.json().then(function (data) {
+//             displayComics(data, user);
+//             console.log(data);
+//           });
+//         } else {
+//           alert('Error: ' + response.statusText);
+//         }
+//       })
+//       .catch(function (error) {
+//         alert('Unable to connect to Comic Vine');
+//       });
+//   };getComics();
