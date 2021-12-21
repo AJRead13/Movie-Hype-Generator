cba6e296a922482c85bc5426b96696e6235b5ff3
Tina Stanczyk <cstanczyk123@gmail.com>
2021-12-21 13:13:22 -0500

    fetching comic api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,3 +16,25 @@ function getOmdbApi(apiURL){
 };
 
 getOmdbApi(apiURL);
+
+// Fetching from Marvel API
+
+var comicApiKey = "87a2a54f6a890d5ea11b4846f3ac4b6c5019ed2d";
+
+var comicApiURL = "http://comicvine.gamespot.com/api/volumes/?api_key="+ comicApiKey + "&format=json";
+
+function getComicApi(apiURL){
+  fetch(apiURL)
+    .then(function(response){
+      if(!response.ok){
+        console.log(response);
+      }
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data);
+    });
+    
+};
+
+getComicApi(comicApiURL);
